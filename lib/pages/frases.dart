95e16616fcfdd297ce7389ffe381763c251dbbb6
Bruno Ancona <brunoanconasala@gmail.com>
2024-01-21 23:00:18 -0600

    idk

--- lib/pages/frases.dart
+++ lib/pages/frases.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class Frases extends StatelessWidget {
+class Frases extends StatefulWidget {
   const Frases({super.key});
+
+  @override
+  State<Frases> createState() => _FrasesState();
+}
+
+class _FrasesState extends State<Frases> {
+  bool revealButtonLoading = false;
+  String loadingButtonText = 'Cargando amor...';
 
   @override
   Widget build(BuildContext context) {
@@ -32,27 +41,72 @@ class Frases extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "Tu mensajito de hoy es:",
+                      'Tu mensajito de hoy es:',
                       style: theme.textTheme.headlineSmall!.copyWith(
                         color: theme.colorScheme.onPrimary,
                       ),
                       textAlign: TextAlign.center,
                     ),
                     const SizedBox(height: 10),
-                    //BigCard(text: "Te amo con todo mi corazoncito bebé preciosa <3"),
+                    //BigCard(text: 'Te amo con todo mi corazoncito bebé preciosa <3'),
                     ElevatedButton(
                       child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          "Revelar",
-                          style: theme.textTheme.displaySmall,
-                          textAlign: TextAlign.center,
-                        ),
+                        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        child: revealButtonLoading
+                          ? FittedBox(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Lottie.asset(
+                                  width: 40,
+                                  height: 40,
+                                  'assets/loading.json'
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  loadingButtonText,
+                                  style: theme.textTheme.headlineSmall,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          )
+                          : FittedBox(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Lottie.asset(
+                                  width: 40,
+                                  height: 40,
+                                  'assets/wired-outline-1865-shooting-stars.json'
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  'Revelar',
+                                  style: theme.textTheme.displaySmall,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
                       ),
                       onPressed: () {
+                        setState(() {
+                          revealButtonLoading = true;
+                        });
 
+                        Future.delayed(const Duration(seconds: 5), () {
+                          setState(() {
+                            loadingButtonText = 'Buscando palabras bonitas...';
+                          });
+
+                          Future.delayed(const Duration(seconds: 5), () {
+                            setState(() {
+                              loadingButtonText = 'Escribiendo tu mensajito...';
+                            });
+                          });
+                        });
                       },
-                      
                     ),
                     const SizedBox(height: 10),
                   ],
