0be4a4fb5e128f9de9898ce9d9489d92393045b7
Bruno Ancona <brunoanconasala@gmail.com>
2024-01-29 21:11:33 -0600

    Ignore text scaling

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,12 @@ class MainApp extends StatelessWidget {
           onSurface: Colors.white,
         ),
       ),
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+          child: child!,
+        );
+      },
       home: const FirstTimeIntro(),
     );
   }
