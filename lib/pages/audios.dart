78d64f1a1718e3d8890f6978a780074a7be9932e
Bruno Ancona <brunoanconasala@gmail.com>
2024-02-03 01:52:29 -0600

    Init audio work

--- lib/pages/audios.dart
+++ lib/pages/audios.dart
@@ -1,4 +1,3 @@
-import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
@@ -37,17 +36,21 @@ final _playlist = ConcatenatingAudioSource(children: [
 class AudioPlayerManager {
   final player = AudioPlayer();
   Stream<DurationState>? durationState;
+  bool startedPlay = false;
 
   void init() {
-    durationState = Rx.combineLatest2<Duration, PlaybackEvent, DurationState>(
-      player.positionStream,
-      player.playbackEventStream,
-      (position, playbackEvent) => DurationState(
+    durationState = Rx.combineLatest4<Duration, PlaybackEvent, int?, bool, DurationState>(
+      player.positionStream.asBroadcastStream(),
+      player.playbackEventStream.asBroadcastStream(),
+      player.currentIndexStream.asBroadcastStream(),
+      player.playingStream.asBroadcastStream(),
+      (position, playbackEvent, currentIndex, playing) => DurationState(
         progress: position,
-        buffered: playbackEvent.bufferedPosition,
         total: playbackEvent.duration,
+        index: currentIndex,
+        playing: playing,
       ),
-    );
+    ).asBroadcastStream();
 
     player.setAudioSource(_playlist);
   }
@@ -60,13 +63,15 @@ class AudioPlayerManager {
 class DurationState {
   const DurationState({
     required this.progress,
-    required this.buffered,
     this.total,
+    this.index,
+    required this.playing,
   });
 
   final Duration progress;
-  final Duration buffered;
   final Duration? total;
+  final int? index;
+  final bool playing;
 }
 
 class Audios extends StatefulWidget {
@@ -100,74 +105,175 @@ class _AudiosState extends State<Audios> with AutomaticKeepAliveClientMixin {
     super.build(context);
 
     //final ThemeData theme = Theme.of(context);
-    audioPlayerManager.player.play();
+    //audioPlayerManager.player.play();
 
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StreamBuilder<DurationState>(
-                stream: audioPlayerManager.durationState,
-                builder: (context, snapshot) {
-                  final durationState = snapshot.data;
-                  final progress = durationState?.progress ?? Duration.zero;
-                  final buffered = durationState?.buffered ?? Duration.zero;
-                  final total = durationState?.total ?? Duration.zero;
-                  return ProgressBar(
-                    progress: progress,
-                    buffered: buffered,
-                    total: total,
-                    onSeek: audioPlayerManager.player.seek,
-                    //baseBarColor: Colors.black,
-                    onDragUpdate: (details) {
-                      //debugPrint('${details.timeStamp}, ${details.localPosition}');
-                    },
-                  );
-                },
-              ),
+    return Scaffold(
+      body: ListView(
+        children: [
+          for (var i = 0; i < (audioPlayerManager.player.sequence?.length ?? 0); i++)
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Row( 
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    StreamBuilder(
+                      stream: audioPlayerManager.durationState,
+                      builder: (context, snapshot) {
+                        final durationState = snapshot.data;
+                        final index = durationState?.index;
+                        final playing = durationState?.playing;
+                        final progress = durationState?.progress ?? Duration.zero;
+                        final total = durationState?.total ?? Duration.zero;
 
-              StreamBuilder<PlayerState>(
-                stream: audioPlayerManager.player.playerStateStream,
-                builder: (context, snapshot) {
-                  final playerState = snapshot.data;
-                  final processingState = playerState?.processingState;
-                  final playing = playerState?.playing;
-                  if (processingState == ProcessingState.loading ||
-                      processingState == ProcessingState.buffering) {
-                    return Container(
-                      margin: const EdgeInsets.all(8.0),
-                      width: 32.0,
-                      height: 32.0,
-                      child: const CircularProgressIndicator(),
-                    );
-                  } else if (playing != true) {
-                    return IconButton(
-                      icon: const Icon(Icons.play_arrow),
-                      iconSize: 32.0,
-                      onPressed: audioPlayerManager.player.play,
-                    );
-                  } else if (processingState != ProcessingState.completed) {
-                    return IconButton(
-                      icon: const Icon(Icons.pause),
-                      iconSize: 32.0,
-                      onPressed: audioPlayerManager.player.pause,
-                    );
-                  } else {
-                    return IconButton(
-                      icon: const Icon(Icons.replay),
-                      iconSize: 32.0,
-                      onPressed: () =>
-                          audioPlayerManager.player.seek(Duration.zero),
-                    );
-                  }
-                },
+                        if (index == i && playing == true && progress < total) {
+                          return IconButton(
+                            icon: const Icon(Icons.pause, size: 30),
+                            onPressed: () async => await audioPlayerManager.player.pause(),
+                          );
+                        }
+                        else {
+                          return IconButton(
+                            icon: const Icon(Icons.play_arrow, size: 30),
+                            onPressed: () {
+                              if (!audioPlayerManager.startedPlay) {
+                                setState(() {
+                                  audioPlayerManager.startedPlay = true;
+                                });
+                              }
+
+                              audioPlayerManager.player.seek(Duration.zero, index: i);
+                              audioPlayerManager.player.play();
+                            },
+                          );
+                        }
+                      }
+                    ),
+
+                    const SizedBox(width: 10),
+                
+                    Expanded(
+                      child: SliderTheme(
+                        data: const SliderThemeData(
+                          trackShape: CustomSliderTrackShape(),
+                          thumbShape: CustomSliderThumbShape(),
+                          overlayShape: CustomSliderOverlayShape(),
+                        ),
+                        child: StreamBuilder<DurationState>(
+                          stream: audioPlayerManager.durationState,
+                          builder: (context, snapshot) {
+                            final durationState = snapshot.data;
+                            double progress = durationState?.progress.inMilliseconds.toDouble() ?? 0;
+                            final total = durationState?.total?.inMilliseconds.toDouble() ?? 0;
+                            final index = durationState?.index;
+                            if (progress > total) {
+                              progress = total;
+                            }
+
+                            return Slider(
+                              value: index == i ? progress : 0,
+                              min: 0,
+                              max: index == i ? total : 0,
+                              onChanged: (index == i) && audioPlayerManager.startedPlay ? (value) async {
+                                final position = Duration(milliseconds: value.toInt());
+                                await audioPlayerManager.player.seek(position);
+                              } : null,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      )
+            ),
+        ],
+      ),
     );
+  }
+}
+
+class CustomSliderTrackShape extends RoundedRectSliderTrackShape {
+  const CustomSliderTrackShape();
+  @override
+  Rect getPreferredRect({
+    required RenderBox parentBox,
+    Offset offset = Offset.zero,
+    required SliderThemeData sliderTheme,
+    bool isEnabled = false,
+    bool isDiscrete = false,
+  }) {
+    final trackHeight = sliderTheme.trackHeight;
+    final trackLeft = offset.dx;
+    final trackTop = offset.dy + (parentBox.size.height - trackHeight!) / 2;
+    final trackWidth = parentBox.size.width;
+    return Rect.fromLTWH(trackLeft, trackTop, trackWidth, trackHeight);
+  }
+}
+
+class CustomSliderThumbShape extends RoundSliderThumbShape {
+  const CustomSliderThumbShape({super.enabledThumbRadius = 10.0});
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {
+    super.paint(context,
+        center.translate(-(value - 0.5) / 0.5 * enabledThumbRadius, 0.0),
+        activationAnimation: activationAnimation,
+        enableAnimation: enableAnimation,
+        isDiscrete: isDiscrete,
+        labelPainter: labelPainter,
+        parentBox: parentBox,
+        sliderTheme: sliderTheme,
+        textDirection: textDirection,
+        value: value,
+        textScaleFactor: textScaleFactor,
+        sizeWithOverflow: sizeWithOverflow);
+  }
+}
+
+class CustomSliderOverlayShape extends RoundSliderOverlayShape {
+  final double thumbRadius;
+  const CustomSliderOverlayShape({this.thumbRadius = 10.0});
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required bool isDiscrete,
+    required TextPainter labelPainter,
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required TextDirection textDirection,
+    required double value,
+    required double textScaleFactor,
+    required Size sizeWithOverflow,
+  }) {
+    super.paint(
+        context, center.translate(-(value - 0.5) / 0.5 * thumbRadius, 0.0),
+        activationAnimation: activationAnimation,
+        enableAnimation: enableAnimation,
+        isDiscrete: isDiscrete,
+        labelPainter: labelPainter,
+        parentBox: parentBox,
+        sliderTheme: sliderTheme,
+        textDirection: textDirection,
+        value: value,
+        textScaleFactor: textScaleFactor,
+        sizeWithOverflow: sizeWithOverflow);
   }
 }
