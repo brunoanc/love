f5f104cb55a03e6a62bd98b8611baacf4b7c1d34
Bruno Ancona <brunoanconasala@gmail.com>
2024-02-05 23:04:34 -0600

    Finish settings

--- lib/pages/configuracion.dart
+++ lib/pages/configuracion.dart
@@ -6,7 +6,6 @@ import '../first_time_intro/onboarding_screen.dart';
 
 class Configuracion extends StatelessWidget {
   const Configuracion({super.key});
-
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -122,56 +121,71 @@ class Configuracion extends StatelessWidget {
                     onPressed: (context) {
                       showGeneralDialog<String>(
                         context: context,
-                        pageBuilder: (context, anim1, anim2) => Dialog.fullscreen(
-                          child: Scaffold(
-                            backgroundColor: Colors.yellow.shade100,
-                            floatingActionButton:  CupertinoButton(
-                              padding: const EdgeInsets.fromLTRB(35, 10, 35, 10),
-                              borderRadius: const BorderRadius.all(Radius.elliptical(100, 100)),
-                              color: Colors.yellow,
-                              onPressed: () => Navigator.pop(context),
-                              child: Text(
-                                'Regresar',
-                                style: theme.textTheme.bodyLarge,
-                                textAlign: TextAlign.center,
+                        pageBuilder: (context, anim1, anim2) {
+                          final datingTime = differenceInString(DateTime(2023, 7, 15), DateTime.now());
+                          final relationshipTime = differenceInString(DateTime(2023, 8, 8), DateTime.now());
+
+                          return Dialog.fullscreen(
+                            child: Scaffold(
+                              backgroundColor: Colors.yellow.shade100,
+                              floatingActionButton:  CupertinoButton(
+                                padding: const EdgeInsets.fromLTRB(35, 10, 35, 10),
+                                borderRadius: const BorderRadius.all(Radius.elliptical(100, 100)),
+                                color: Colors.yellow,
+                                onPressed: () => Navigator.pop(context),
+                                child: Text(
+                                  'Regresar',
+                                  style: theme.textTheme.bodyLarge,
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
-                            ),
-                            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-                            body: SafeArea(
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(24),
-                                        child: Image.asset(
-                                          'assets/images/nosotras.jpg',
-                                          fit: BoxFit.fill,
-                                          //width: imageDimension,
-                                          //height: imageDimension,
+                              floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+                              body: SafeArea(
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 80),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(24),
+                                          child: Image.asset(
+                                            'assets/images/nosotras.jpg',
+                                            fit: BoxFit.fill,
+                                            //width: imageDimension,
+                                            //height: imageDimension,
+                                          ),
                                         ),
-                                      ),
 
-                                      const SizedBox(height: 10),
+                                        const SizedBox(height: 10),
 
-                                      Text(
-                                        'hoy y siempre',
-                                        style: theme.textTheme.headlineMedium!.copyWith(
-                                          color: Colors.black,
-                                          fontFamily: 'Fearless'
+                                        Text(
+                                          'hoy y siempre',
+                                          style: theme.textTheme.headlineMedium!.copyWith(
+                                            color: Colors.black,
+                                            fontFamily: 'Folklore'
+                                          ),
+                                          textAlign: TextAlign.center,
                                         ),
-                                        textAlign: TextAlign.center,
-                                        maxLines: 1,
-                                      ),
-                                    ],
+
+                                        const SizedBox(height: 10),
+
+                                        Text(
+                                          '$datingTime saliendo\n$relationshipTime juntas\nMuchísimos años por explorar y amar agarradas de la manita <3',
+                                          style: theme.textTheme.headlineSmall!.copyWith(
+                                            color: Colors.black,
+                                            fontFamily: 'Bruno'
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
+                          );
+                        },
                         transitionBuilder: (context, anim1, anim2, child) {
                           return FadeTransition(
                             opacity: anim1.drive(
@@ -289,4 +303,78 @@ class Configuracion extends StatelessWidget {
       ),
     );
   }
+}
+
+bool leapYear(DateTime date) {
+  if (date.year % 4 == 0) {
+    if (date.year % 100 == 0){
+      return date.year % 400 == 0;
+    }
+    return true;
+  }
+  return false;
+}
+
+List<int> differenceInYearsMonthsDays(DateTime dt1, DateTime dt2) {
+  List<int> simpleYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31];
+  if (dt1.isAfter(dt2)) {
+    DateTime temp = dt1;
+    dt1 = dt2;
+    dt2 = temp;
+  }
+  int totalMonthsDifference = ((dt2.year * 12) + (dt2.month - 1)) - ((dt1.year * 12) + (dt1.month - 1));
+  int years = (totalMonthsDifference / 12).floor();
+  int months = totalMonthsDifference % 12;
+  late int days;
+  if (dt2.day >= dt1.day) {
+    days = dt2.day - dt1.day;
+  }
+  else {
+    int monthDays = dt2.month == 3
+        ? (leapYear(dt2)? 29 : 28)
+        : (dt2.month - 2 == -1 ? simpleYear[11]: simpleYear[dt2.month - 2]);
+    int day = dt1.day;
+    if (day > monthDays) {
+      day = monthDays;
+    }
+    days = monthDays - (day - dt2.day);
+    months--;
+  }
+  if (months < 0) {
+    months = 11;
+    years--;
+  }
+  return [years, months, days];
+}
+
+String differenceInString(DateTime dt1, DateTime dt2) {
+  final yearsMonthsDays = differenceInYearsMonthsDays(dt1, dt2);
+  var differenceList = <String>[];
+  if (yearsMonthsDays[0] > 0) {
+    if (yearsMonthsDays[0] == 1) {
+      differenceList.add('1 año');
+    }
+    else {
+      differenceList.add('${yearsMonthsDays[0]} años');
+    }
+  }
+  if (yearsMonthsDays[1] > 0) {
+    if (yearsMonthsDays[1] == 1) {
+      differenceList.add('1 mes');
+    }
+    else {
+      differenceList.add('${yearsMonthsDays[1]} meses');
+    }
+  }
+  if (yearsMonthsDays[2] > 0) {
+    if (yearsMonthsDays[2] == 1) {
+      differenceList.add('1 día');
+    }
+    else {
+      differenceList.add('${yearsMonthsDays[2]} días');
+    }
+  }
+  var differenceString = differenceList.join(', ');
+  var lastComma = differenceString.lastIndexOf(', ').clamp(0, differenceString.length);
+  return differenceString.replaceFirst(', ', ' y ', lastComma);
 }
